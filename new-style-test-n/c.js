1cdb223bab5724374f4d18d3f3990af9c34df289
meteturk <152897496+meteturkg@users.noreply.github.com>
2025-10-02 20:22:06 +0300

    Create c.js

---
+++ new-style-test-n/c.js
@@ -0,0 +1,9 @@
+const config = {
+  name: "meteturkg/meteturkdev",
+  about: "I'm crazy, yes yes",
+  avatar: "https://github.com/meteturkg.png",
+  rainbow: true,
+  rainbowSpeed: 5,  // saniye cinsinden
+  background: "#E3E3E3",
+  buttonBackground: "#FFFFFF"
+};
