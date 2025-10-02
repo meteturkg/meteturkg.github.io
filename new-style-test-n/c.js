97076f886897d05bb8ea08de36e4918c03850d6c
meteturk <152897496+meteturkg@users.noreply.github.com>
2025-10-02 20:23:57 +0300

    Update c.js

--- new-style-test-n/c.js
+++ new-style-test-n/c.js
@@ -1,9 +1,11 @@
 const config = {
+  s_name: "*/",
+  background: "#E3E3E3",
+  buttonBackground: "#FFFFFF",
+  rainbow: true,
   name: "meteturkg/meteturkdev",
   about: "I'm crazy, yes yes",
   avatar: "https://github.com/meteturkg.png",
-  rainbow: true,
-  rainbowSpeed: 5,  // saniye cinsinden
-  background: "#E3E3E3",
-  buttonBackground: "#FFFFFF"
+  themeColor: "#E7ECE8",
+  showLoading: true
 };
